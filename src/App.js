cd418ac4b07f89b893fef5d901ac6047c9dde7b4
Matheus Frauches <frauches.27@gmail.com>
2019-11-11 22:35:17 -0300

    Add useDelete hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useGet } from './useGet';
 import { usePost } from './usePost';
+import { useDelete } from './useDelete';
 
 function App() {
 
   const url = 'https://mymoney-3ea9b.firebaseio.com/movimentacoes/2019-10.json';
   const data = useGet(url);
   const [postData, post] = usePost(url);
+  const [deleteData, remove] = useDelete();
 
   const saveData = () => {
     post({ valor: 7, descricao: 'Coxinha com Catupiry' });
+  }
+
+  const doRemove = () => {
+    remove('https://mymoney-3ea9b.firebaseio.com/movimentacoes/2019-10/-LrHJqnEmvxd4KFiCA9Z.json');
   }
 
   return (
@@ -20,6 +26,8 @@ function App() {
 
       <button onClick={saveData}>Salvar</button>
       <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Delete</button>  
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   );
 }
